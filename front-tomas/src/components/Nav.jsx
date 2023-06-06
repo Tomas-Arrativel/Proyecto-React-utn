c4b3dbdc7bb11a5e9171d2307f48206762e6b706
Tomás Arrativel <arrativeltomas@gmail.com>
2023-06-06 09:40:08 -0300

    clase active a navlink

--- front-tomas/src/components/Nav.jsx
+++ front-tomas/src/components/Nav.jsx
@@ -1,26 +1,26 @@
 import '../styles/components/normalize.css';
 import '../styles/components/layout/Nav.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav>
       <ul className='header__list'>
-        <Link to='/' className='header__list-links active'>
+        <NavLink to='/' className='header__list-links'>
           <li>Inicio</li>
-        </Link>
-        <Link to='/nosotros' className='header__list-links'>
+        </NavLink>
+        <NavLink to='/nosotros' className='header__list-links'>
           <li>Nosotros</li>
-        </Link>
-        <Link to='/novedades' className='header__list-links'>
+        </NavLink>
+        <NavLink to='/novedades' className='header__list-links'>
           <li>Noticias</li>
-        </Link>
-        <Link to='/servicios' className='header__list-links'>
+        </NavLink>
+        <NavLink to='/servicios' className='header__list-links'>
           <li>Servicios</li>
-        </Link>
-        <Link to='/contacto' className='header__list-links'>
+        </NavLink>
+        <NavLink to='/contacto' className='header__list-links'>
           <li>Contacto</li>
-        </Link>
+        </NavLink>
       </ul>
     </nav>
   );
