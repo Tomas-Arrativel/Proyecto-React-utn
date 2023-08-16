e9e227d7a2fb9c2c1fc7d0742992fdb5297a7549
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-16 10:40:21 -0300

    Parte de envio de mail en el front

--- front-tomas/src/pages/ContactoPage.jsx
+++ front-tomas/src/pages/ContactoPage.jsx
@@ -1,34 +1,100 @@
 import '../styles/components/normalize.css';
 import '../styles/components/pages/HomePage.css';
 import '../styles/components/pages/ContactoPage.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function ContactoPage() {
+  const initialForm = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    mensaje: '',
+  };
+
+  const [sending, setSending] = useState(false);
+  const [msg, setMsg] = useState('');
+  const [formData, setFormData] = useState(initialForm);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((oldData) => ({
+      ...oldData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMsg('');
+    setSending(true);
+    const response = await axios.post(
+      'http://localhost:3000/api/contacto',
+      formData,
+    );
+    setSending(false);
+    setMsg(response.data.message);
+    if (response.data.error === false) {
+      setFormData(initialForm);
+    }
+  };
+
   return (
     <main className='main__contacto'>
       <div className='main__contacto-formulario'>
         <div className='formulario'>
           <h1>Contacto</h1>
           <h2>Completa el Formulario</h2>
-          <form className='form'>
+          <form
+            action='/contacto'
+            method='post'
+            className='form'
+            onSubmit={handleSubmit}
+          >
             <div>
-              <label>Nombre</label>
-              <input type='text' placeholder='Nombre' />
+              <label htmlFor='nombre'>Nombre</label>
+              <input
+                type='text'
+                placeholder='Nombre'
+                name='nombre'
+                value={formData.nombre}
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <label>Email</label>
-              <input type='email' placeholder='Email' />
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                placeholder='Email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <label>Telefono</label>
-              <input type='text' placeholder='Telefono' />
+              <label htmlFor='telefono'>Telefono</label>
+              <input
+                type='text'
+                placeholder='Telefono'
+                name='telefono'
+                value={formData.telefono}
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <label>Mensaje</label>
-              <textarea placeholder='Su mensaje aqui...'></textarea>
+              <label htmlFor='mensaje'>Mensaje</label>
+              <textarea
+                placeholder='Su mensaje aqui...'
+                name='mensaje'
+                value={formData.mensaje}
+                onChange={handleChange}
+              ></textarea>
             </div>
 
-            <input type='button' value='Enviar' className='form-btn' />
+            <input type='submit' value='Enviar' className='form-btn' />
           </form>
+          {sending ? <p>Enviando...</p> : null}
+          {msg ? <p>{msg}</p> : null}
         </div>
 
         <div className='mas-formas'>
