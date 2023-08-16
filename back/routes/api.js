2519573c21037179429a8c574c1ad4f2bdbf901d
Tomás Arrativel <arrativeltomas@gmail.com>
2023-08-16 10:40:05 -0300

    agrego la parte de envio de mail en el back

--- back/routes/api.js
+++ back/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const novedadesModel = require('./../models/novedadesModel');
 const cloudinary = require('cloudinary');
+const nodemailer = require('nodemailer');
 
 router.get('/novedades', async (req, res, next) => {
   let novedades = await novedadesModel.getNovedades();
@@ -25,6 +26,32 @@ router.get('/novedades', async (req, res, next) => {
   });
 
   res.json(novedades);
+});
+
+// NodeMailer
+router.post('/contacto', async (req, res) => {
+  const mail = {
+    to: 'arrativeltomas@gmail.com',
+    subject: 'Mensaje desde CryptoWeb',
+    html: `${req.body.nombre} se contacto a traves de la web con el correo de: ${req.body.email}
+    <br> Ademas, comentó lo siguiente: ${req.body.mensaje}. <br> Su numero de telefono es: ${req.body.telefono}`,
+  };
+
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  await transport.sendMail(mail);
+
+  res.status(201).json({
+    error: false,
+    message: 'Mensaje enviado',
+  });
 });
 
 module.exports = router;
